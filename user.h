08eb2ce1c58a9696492c0a987fdbbf1334b57346
Zach Hamby <150568022+zhamby@users.noreply.github.com>
2024-09-26 16:46:08 +0000

    add function outline user.h

--- user.h
+++ user.h
@@ -1,1 +1,33 @@
+#ifndef USER_H
+#define USER_H
 
+#include<iostream>
+#include<string.h>
+#include<vector>
+
+class User {
+    private:
+        std::string userName;
+        std::string email;
+        std::vector<User*> friendList;
+        
+    public:
+        //Setters and Getters for UserName and Email
+        void setUserName(std::string userName);
+
+        void setEmail(std::string username);
+
+        std::string getUserName();
+
+        std::string getEmail();
+
+        void addFriend(User*); //take a user pointer and add to friendList
+
+        void removeFriend(std::string userName); //take username as string and remove from friendList
+        
+        int numFriend(); //return number of friends in friendList
+        
+        User* getFriendAt(int index); //return user pointer when given an index position
+};
+
+#endif
