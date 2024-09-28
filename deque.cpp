8f5afa96f6ff7083a5f766e4e43e459de07f3e01
Zach Hamby <150568022+zhamby@users.noreply.github.com>
2024-09-28 04:56:58 +0000

    add function implementation deque

--- deque.cpp
+++ deque.cpp
@@ -1,1 +1,124 @@
+#include "deque.h"
+
+// Constructor to initialize an empty deque
+Deque::Deque(int cap) : front(nullptr), back(nullptr), size(0), capacity(cap) {}
+
+// Destructor to clean up memory
+Deque::~Deque() {
+    remove_all();
+}
+
+
+void Deque::push_front(int value) {
+    if (size == capacity) {
+        throw std::overflow_error("Deque is full");
+    }
+
+    Node* new_node = new Node(value);
+    
+    if (size == 0) {
+        front = back = new_node;  // If deque is empty, front and back are the same
+    } else {
+        new_node->next = front;
+        front->prev = new_node;
+        front = new_node;
+    }
+
+    size++;
+}
+
+void Deque::push_back(int value) {
+    if (size == capacity) {
+        throw std::overflow_error("Deque is full");
+    }
+
+    Node* new_node = new Node(value);
+
+    if (size == 0) {
+        front = back = new_node;  // If deque is empty, front and back are the same
+    } else {
+        new_node->prev = back;
+        back->next = new_node;
+        back = new_node;
+    }
+
+    size++;
+}
+
+int Deque::pop_front() {
+    if (size == 0) {
+        throw std::underflow_error("Deque is empty");
+    }
+
+    int value = front->value;
+    Node* temp = front;
+
+    if (front == back) {
+        front = back = nullptr;  // If there's only one node, deque becomes empty
+    } else {
+        front = front->next;
+        front->prev = nullptr;
+    }
+
+    delete temp; //free memory
+    size--;
+
+    return value;
+}
+
+// Pop and return the back element of the deque
+int Deque::pop_back() {
+    if (size == 0) {
+        throw std::underflow_error("Deque is empty");
+    }
+
+    int value = back->value;
+    Node* temp = back;
+
+    if (front == back) {
+        front = back = nullptr;  // If there's only one node, deque becomes empty
+    } else {
+        back = back->prev;
+        back->next = nullptr;
+    }
+
+    delete temp;
+    size--;
+
+    return value;
+}
+
+// Peek at the front element without removing it
+int Deque::peak_front() const {
+    if (size == 0) {
+        throw std::underflow_error("Deque is empty");
+    }
+
+    return front->value;
+}
+
+// Peek at the back element without removing it
+int Deque::peak_back() const {
+    if (size == 0) {
+        throw std::underflow_error("Deque is empty");
+    }
+
+    return back->value;
+}
+
+// Remove all elements and free memory
+void Deque::remove_all() {
+    while (front != nullptr) {
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+    }
+    back = nullptr;
+    size = 0;
+}
+
+// Get the current size of the deque
+int Deque::get_size() const {
+    return size;
+}
 
--- deque.h
+++ deque.h
@@ -1,1 +1,56 @@
+#ifndef DEQUE_H
+#define DEQUE_H
 
+#include <iostream>
+#include <stdexcept>  // For exception handling
+
+// Node struct for the doubly linked list
+struct Node {
+    int value;
+    Node* prev;
+    Node* next;
+
+    Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+};
+
+// Deque class definition
+class Deque {
+private:
+    Node* front;   // Pointer to the front node
+    Node* back;    // Pointer to the back node
+    int size;       // Current size of the deque
+    int capacity;  // Maximum capacity of the deque
+
+public:
+    // Constructor to initialize an empty deque
+    Deque(int cap = 1000);
+
+    // Destructor to clean up memory
+    ~Deque();
+
+    // Push an element to the front of the deque
+    void push_front(int value);
+
+    //Push an element to the back of the deque
+    void push_back(int value);
+
+    //Pop and return the front element of the deque
+    int pop_front();
+
+    // Pop and return the back element of the deque
+    int pop_back();
+
+    //Peek at the front element without removing it
+    int peak_front() const;
+
+    //Peek at the back element without removing it
+    int peak_back() const;
+
+    //Remove all elements and free memory
+    void remove_all();
+
+    //Get the current size of the deque
+    int get_size() const;
+};
+
+#endif
